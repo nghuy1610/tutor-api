5e4847fef9a97de4ecae0ceecc1d17379371ec58
Huy Ha <nghuy1610@gmail.com>
2021-05-19 00:31:48 +0700

    Init project

---
+++ tutor/src/main/java/vn/tutor/core/TutorCoreApplication.java
@@ -0,0 +1,13 @@
+package vn.tutor.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorCoreApplication.class, args);
+    }
+
+}
---
+++ tutor/src/test/java/vn/tutor/core/TutorCoreApplicationTests.java
@@ -0,0 +1,16 @@
+package vn.tutor.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class TutorCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertThat(true).isTrue();
+    }
+
+}
