7a077850334ed8a6fa67d46ab9015deaa9606f95
Huy Ha <nghuy1610@gmail.com>
2021-06-01 22:51:15 +0700

    Add custom DateTimeProvider bean to allow JpaAuditing work with ZonedDateTime

--- tutor/src/main/java/vn/tutor/core/config/PersistenceConfig.java
+++ tutor/src/main/java/vn/tutor/core/config/PersistenceConfig.java
@@ -1,10 +1,18 @@
 package vn.tutor.core.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@Configuration
-@EnableJpaAuditing
-public class PersistenceConfig {
+import java.time.ZonedDateTime;
+import java.util.Optional;
 
+@Configuration
+@EnableJpaAuditing(dateTimeProviderRef = "dateTimeProvider")
+public class PersistenceConfig {
+    @Bean
+    DateTimeProvider dateTimeProvider() {
+        return () -> Optional.of(ZonedDateTime.now());
+    }
 }
