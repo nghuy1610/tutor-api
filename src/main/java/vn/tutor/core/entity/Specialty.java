509f180bf16bcccfcd41e74304fde51e986565bc
Huy Ha <nghuy1610@gmail.com>
2024-08-13 09:16:01 +0700

    Add checkstyle rule for unused import

--- src/main/java/vn/tutor/core/entity/Specialty.java
+++ src/main/java/vn/tutor/core/entity/Specialty.java
@@ -4,8 +4,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import vn.tutor.core.dto.request.SpecialtyReq;
 
